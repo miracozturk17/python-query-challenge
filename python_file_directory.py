7c7b907349c5ef73cab7e5ea48caed50710dcecd
Miraç  ÖZTÜRK <32125234+miracozturk17@users.noreply.github.com>
2023-04-09 18:47:19 +0300

    python_file_directory
    python_file_directory

---
+++ python_file_directory.py
@@ -0,0 +1,7 @@
+#FARKLI ZAMAN DILIMLERINDE PYTHON UYGULAMASININ KLASOR DIZININI BULAMAYABILIRIZ.
+#HIZLICA KOD TABANLI OLARAK BU BILGIYE ULASABILIRIZ.
+
+import sys
+locate_python = sys.exec_prefix
+print(locate_python)
+#C:\Users\Mıracle\AppData\Local\Programs\Python\Python3
